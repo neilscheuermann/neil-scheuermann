bbbafc6d5b2863305e07f973e9b8c4981c07ca22
Neil Scheuermann <neilscheuermann@gmail.com>
2019-10-16 21:07:33 -0600

    Initial set up.

---
+++ src/pages/about.js
@@ -0,0 +1,14 @@
+import React, { Fragment } from "react"
+
+const AboutPage = () => (
+  <Fragment>
+    <h1>Learn a bit about Neil.</h1>
+    <span>
+      <a href="https://www.linkedin.com/in/neil-scheuermann/" target="_blank">
+        LinkedIn
+      </a>
+    </span>
+  </Fragment>
+)
+
+export default AboutPage
